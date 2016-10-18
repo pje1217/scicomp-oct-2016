d6c0a2c4acfae475e843f5cecf976e67a8073d18
Jieun Esther Park <jieun.e.park@duke.edu>
2016-10-18 12:12:30 -0400

    changes made

--- read_fasta.py
+++ read_fasta.py
@@ -16,6 +16,6 @@ if len(sys.argv) < 2:
     print "Usage:", sys.argv[0], "<sequence.fa>"
     exit(1)
 
-print read_fasta(sys.argv[1])
+print sys.argv[1], ':', read_fasta(sys.argv[1])
 
 ## end of analysis
